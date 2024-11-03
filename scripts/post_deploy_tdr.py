104a73bde6f7b469f19eae20f0969f5c348ab37c
Noa Aviel Dove <nadove@ucsc.edu>
2024-11-03 15:57:37 -0800

    [r] Index orphaned replicas (#6626)

--- scripts/post_deploy_tdr.py
+++ scripts/post_deploy_tdr.py
@@ -96,12 +96,12 @@ class TerraValidator:
         plugin = self.repository_plugin(catalog)
         ref = plugin.resolve_source(str(source_spec))
         log.info('TDR client is authorized for API access to %s.', source_spec)
-        ref = plugin.partition_source(catalog, ref)
-        prefix = ref.spec.prefix
         if config.deployment.is_main:
-            require(prefix.common == '', source_spec)
+            if source_spec.prefix is not None:
+                require(source_spec.prefix.common == '', source_spec)
             self.tdr.check_bigquery_access(source_spec)
         else:
+            ref = plugin.partition_source(catalog, ref)
             subgraph_count = plugin.count_bundles(ref.spec)
             require(subgraph_count > 0, 'Common prefix is too long', ref.spec)
             require(subgraph_count <= 512, 'Common prefix is too short', ref.spec)
