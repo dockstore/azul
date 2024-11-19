bc7f533fefaf2f98931c7a2ca085e5cea772bc23
Noa Aviel Dove <nadove@ucsc.edu>
2024-11-18 20:13:40 -0800

    Document Swagger update procedure and add script (#6546)

---
+++ scripts/update_swagger.py
@@ -0,0 +1,55 @@
+import logging
+from pathlib import (
+    Path,
+)
+
+from furl import (
+    furl,
+)
+
+from azul import (
+    config,
+    require,
+)
+from azul.http import (
+    http_client,
+)
+from azul.logging import (
+    configure_script_logging,
+)
+
+log = logging.getLogger(__name__)
+http = http_client(log)
+
+repository_url = 'https://raw.githubusercontent.com/swagger-api/swagger-ui'
+tag = 'v4.15.2'
+files = [
+    'index.html',
+    'swagger-ui.css',
+    'swagger-ui-bundle.js',
+    'swagger-ui-standalone-preset.js',
+    # We don't directly serve these files, but we maintain verbatim copies from
+    # the upstream distribution for reference.
+    'oauth2-redirect.html',
+    'swagger-initializer.js'
+]
+
+swagger_dir = Path(config.project_root) / 'swagger'
+
+
+def download_file(name: str):
+    object_url = furl(repository_url) / tag / 'dist' / name
+    response = http.request('GET', str(object_url))
+    require(response.status == 200, name)
+    with open(swagger_dir / name, 'wb') as f:
+        f.write(response.data)
+
+
+def main():
+    for file_name in files:
+        download_file(file_name)
+
+
+if __name__ == '__main__':
+    configure_script_logging(log)
+    main()
