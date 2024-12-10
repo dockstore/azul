63c9a4f81be7a0aa6b59543cbd9359d0300e4b53
Noa Aviel Dove <nadove@ucsc.edu>
2024-12-09 19:53:38 -0800

    Update AWS TF provider (5.49 -> 5.80)

--- terraform/providers.tf.json.template.py
+++ terraform/providers.tf.json.template.py
@@ -23,7 +23,7 @@ emit_tf(tag_resources=False, config={
             },
             'aws': {
                 'source': 'hashicorp/aws',
-                'version': '5.49.0'
+                'version': '5.80.0'
             },
             # FIXME: Remove the provider
             #        https://github.com/DataBiosphere/azul/pull/6285
