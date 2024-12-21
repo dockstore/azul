57c295071c01b6e0da13183a5b6322031992b172
Hannes Schmidt <hannes@ucsc.edu>
2024-12-21 13:31:54 -0800

    Upgrade ES instance type in lower deployments (#6731)

--- deployments/sandbox/environment.py
+++ deployments/sandbox/environment.py
@@ -251,8 +251,7 @@ def env() -> Mapping[str, Optional[str]]:
 
         **(
             {
-                # $0.382/h × 2 × 24h/d × 30d/mo = $550.08/mo
-                'AZUL_ES_INSTANCE_TYPE': 'r6gd.xlarge.elasticsearch',
+                'AZUL_ES_INSTANCE_TYPE': 'r7gd.xlarge.elasticsearch',
                 'AZUL_ES_INSTANCE_COUNT': '2',
             } if is_sandbox else {
                 # Personal deployments share an ES domain with `sandbox`
