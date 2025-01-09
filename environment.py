3b146ea14630e66e5d60b6606aa1d67812183c06
Abraham Chavez <achave11@ucsc.edu>
2025-01-09 11:23:15 -0800

    Update Elasticsearch to 7.17.27-35 (#6805)

--- environment.py
+++ environment.py
@@ -301,7 +301,7 @@ def env() -> Mapping[str, Optional[str]]:
                 'is_custom': True
             },
             'elasticsearch': {
-                'ref': 'docker.io/ucscgi/azul-elasticsearch:7.17.26-33',
+                'ref': 'docker.io/ucscgi/azul-elasticsearch:7.17.27-35',
                 'url': 'https://hub.docker.com/repository/docker/ucscgi/azul-elasticsearch',
                 'is_custom': True
             },
