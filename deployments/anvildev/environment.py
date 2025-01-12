f41fdd99988e58a8786d52e995302b7c5e82c670
Hannes Schmidt <hannes@ucsc.edu>
2025-01-12 15:02:57 -0800

    Remove redundant overrides

--- deployments/anvildev/environment.py
+++ deployments/anvildev/environment.py
@@ -90,7 +90,6 @@ def env() -> Mapping[str, Optional[str]]:
         'AZUL_DEPLOYMENT_STAGE': 'anvildev',
 
         'AZUL_DOMAIN_NAME': 'anvil.gi.ucsc.edu',
-        'AZUL_PRIVATE_API': '0',
 
         'AZUL_CATALOGS': json.dumps({
             f'{catalog}{suffix}': dict(atlas=atlas,
