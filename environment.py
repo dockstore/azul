29dce00f0c9d4bab7fb35a0e58a54bd719cc70ba
Abraham Chavez <achave11@ucsc.edu>
2025-03-11 10:48:08 -0700

    Update GitLab to 17.9.2-ce.0 and runner to ubuntu-v17.9.1 (#6943)

--- environment.py
+++ environment.py
@@ -317,11 +317,11 @@ def env() -> Mapping[str, Optional[str]]:
                 'url': 'https://hub.docker.com/r/clamav/clamav'
             },
             'gitlab': {
-                'ref': 'docker.io/gitlab/gitlab-ce:17.8.2-ce.0',
+                'ref': 'docker.io/gitlab/gitlab-ce:17.9.2-ce.0',
                 'url': 'https://hub.docker.com/r/gitlab/gitlab-ce'
             },
             'gitlab_runner': {
-                'ref': 'docker.io/gitlab/gitlab-runner:ubuntu-v17.8.3',
+                'ref': 'docker.io/gitlab/gitlab-runner:ubuntu-v17.9.1',
                 'url': 'https://hub.docker.com/r/gitlab/gitlab-runner'
             },
             'dind': {
