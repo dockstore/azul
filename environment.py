80145467e2b224e52bfc6558f9e0e9d5f9acbcf5
Daniel Sotirhos <dsotirho@ucsc.edu>
2025-04-29 17:05:41 -0700

    Update GitLab to 17.11.1-ce.0 and runner to v17.11.0 (#7078)

--- environment.py
+++ environment.py
@@ -317,11 +317,11 @@ def env() -> Mapping[str, Optional[str]]:
                 'url': 'https://hub.docker.com/r/clamav/clamav'
             },
             'gitlab': {
-                'ref': 'docker.io/gitlab/gitlab-ce:17.10.4-ce.0',
+                'ref': 'docker.io/gitlab/gitlab-ce:17.11.1-ce.0',
                 'url': 'https://hub.docker.com/r/gitlab/gitlab-ce'
             },
             'gitlab_runner': {
-                'ref': 'docker.io/gitlab/gitlab-runner:ubuntu-v17.10.1',
+                'ref': 'docker.io/gitlab/gitlab-runner:ubuntu-v17.11.0',
                 'url': 'https://hub.docker.com/r/gitlab/gitlab-runner'
             },
             'dind': {
