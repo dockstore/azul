5726a8bc7a2caa493c586267ac5689d7f336b183
Hannes Schmidt <hannes@ucsc.edu>
2025-05-14 11:02:45 -0700

    [H] Extract methods (#7099)

--- test/indexer/test_mirror_controller.py
+++ test/indexer/test_mirror_controller.py
@@ -89,68 +89,82 @@ class TestMirrorController(DCP2TestCase, LocalAppTestCase, WorkQueueTestCase, S3
         self._create_test_bucket(self.bucket)
 
         with self.subTest('remote_mirror'):
-            self.client.remote_mirror(self.catalog, [self.source])
-            source_message = one(self._read_queue(self.client.mirror_queue()))
-            expected_message = dict(action='mirror_source',
-                                    catalog=self.catalog,
-                                    source=self.source.to_json())
-            self.assertEqual(expected_message, source_message)
+            source_message = self._test_remote_mirror()
 
         with self.subTest('mirror_source'):
-            event = [self._mock_sqs_record(source_message)]
-            self.mirror_controller.mirror(event)
-            partition_messages = self._read_queue(self.client.mirror_queue())
-            partition_message = copy_json(partition_messages[0])
-            partitions = []
-            for message in partition_messages:
-                partitions.append(message.pop('prefix'))
-                self.assertEqual(dict(action='mirror_partition',
-                                      catalog=self.catalog,
-                                      source=self.source.to_json()),
-                                 message)
-            self.assertEqual(list(self.source.spec.prefix.partition_prefixes()), partitions)
-
-        file_contents = b'lorem ipsum dolor sit\n'
+            partition_message = self._test_mirror_source(source_message)
 
         with self.subTest('mirror_partition'):
-            event = [self._mock_sqs_record(partition_message)]
-            file = HCAFile(uuid='405852c9-a0cc-4cd8-b9ff-7c6296223661',
-                           name='foo.txt',
-                           version=None,
-                           drs_uri='drs://fake-domain.lan/foo',
-                           size=len(file_contents),
-                           content_type='text/plain',
-                           sha256=hashlib.sha256(file_contents).hexdigest())
-            plugin_cls = type(self.client.repository_plugin(self.catalog))
-            with patch.object(plugin_cls, 'list_files', return_value=[file]):
-                self.mirror_controller.mirror(event)
-            file_message = one(self._read_queue(self.client.mirror_queue()))
-            expected_message = dict(action='mirror_file',
-                                    catalog=self.catalog,
-                                    source=self.source.to_json(),
-                                    file=file.to_json())
-            self.assertEqual(expected_message, file_message)
+            file, file_message = self._test_mirror_partition(partition_message)
 
         with self.subTest('mirror_file'):
-            event = [self._mock_sqs_record(file_message)]
-            with patch.object(MirrorService, '_download', return_value=file_contents):
-                self.mirror_controller.mirror(event)
-            response = self._s3.get_object(Bucket=self.bucket,
-                                           Key=self.mirror_controller.service.mirror_object_key(file))
-            mirrored_file_contents = response['Body'].read()
-            self.assertEqual(mirrored_file_contents, file_contents)
+            self._test_mirror_file(file, file_message)
 
-            corrupted_contents = file_contents[:-1] + b'Q'
-            with patch.object(MirrorService, '_download', return_value=corrupted_contents):
-                # Force reupload attempt in spite of info object being present
-                with patch.object(MirrorService, '_check_info', return_value=False):
-                    with self.assertRaises(AssertionError) as e:
-                        self.mirror_controller.mirror(event)
-                self.assertTrue(R.caused(e.exception))
+    _file_contents = b'lorem ipsum dolor sit\n'
 
     @property
     def mirror_controller(self) -> MirrorController:
         return self.app_module.app.mirror_controller
+
+    def _test_remote_mirror(self):
+        self.client.remote_mirror(self.catalog, [self.source])
+        source_message = one(self._read_queue(self.client.mirror_queue()))
+        expected_message = dict(action='mirror_source',
+                                catalog=self.catalog,
+                                source=self.source.to_json())
+        self.assertEqual(expected_message, source_message)
+        return source_message
+
+    def _test_mirror_source(self, source_message):
+        event = [self._mock_sqs_record(source_message)]
+        self.mirror_controller.mirror(event)
+        partition_messages = self._read_queue(self.client.mirror_queue())
+        partition_message = copy_json(partition_messages[0])
+        partitions = []
+        for message in partition_messages:
+            partitions.append(message.pop('prefix'))
+            self.assertEqual(dict(action='mirror_partition',
+                                  catalog=self.catalog,
+                                  source=self.source.to_json()),
+                             message)
+        self.assertEqual(list(self.source.spec.prefix.partition_prefixes()), partitions)
+        return partition_message
+
+    def _test_mirror_partition(self, partition_message):
+        event = [self._mock_sqs_record(partition_message)]
+        file = HCAFile(uuid='405852c9-a0cc-4cd8-b9ff-7c6296223661',
+                       name='foo.txt',
+                       version=None,
+                       drs_uri='drs://fake-domain.lan/foo',
+                       size=len(self._file_contents),
+                       content_type='text/plain',
+                       sha256=hashlib.sha256(self._file_contents).hexdigest())
+        plugin_cls = type(self.client.repository_plugin(self.catalog))
+        with patch.object(plugin_cls, 'list_files', return_value=[file]):
+            self.mirror_controller.mirror(event)
+        file_message = one(self._read_queue(self.client.mirror_queue()))
+        expected_message = dict(action='mirror_file',
+                                catalog=self.catalog,
+                                source=self.source.to_json(),
+                                file=file.to_json())
+        self.assertEqual(expected_message, file_message)
+        return file, file_message
+
+    def _test_mirror_file(self, file, file_message):
+        event = [self._mock_sqs_record(file_message)]
+        with patch.object(MirrorService, '_download', return_value=(self._file_contents)):
+            self.mirror_controller.mirror(event)
+        response = self._s3.get_object(Bucket=self.bucket,
+                                       Key=self.mirror_controller.service.mirror_object_key(file))
+        mirrored_file_contents = response['Body'].read()
+        self.assertEqual(mirrored_file_contents, self._file_contents)
+        corrupted_contents = self._file_contents[:-1] + b'Q'
+        with patch.object(MirrorService, '_download', return_value=corrupted_contents):
+            # Force reupload attempt in spite of info object being present
+            with patch.object(MirrorService, '_check_info', return_value=False):
+                with self.assertRaises(AssertionError) as e:
+                    self.mirror_controller.mirror(event)
+            self.assertTrue(R.caused(e.exception))
 
     def test_info_schema(self):
         client = http_client(log)
