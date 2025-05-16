5bbf54ad2fc817506bea96a72da5af97c6b8ae7b
Abraham Chavez <achave11@ucsc.edu>
2025-05-16 13:01:15 -0700

    Make test asserts of expected and actual consistent

--- test/service/test_app_logging.py
+++ test/service/test_app_logging.py
@@ -59,24 +59,27 @@ class TestServiceAppLogging(DCP1CannedBundleTestCase, LocalAppTestCase):
                         **AzulChaliceApp.security_headers(),
                         'Cache-Control': 'no-store'
                     }
-                    self.assertEqual(logs, [
-                        (
-                            INFO,
-                            "Received GET request for '/health/basic', "
-                            f'with {json.dumps(dict(query=None, headers=request_headers))}.'),
-                        (
-                            INFO,
-                            "Authenticated request as OAuth2(access_token='foo_token')"
-                            if authenticated else
-                            'Did not authenticate request.'
-                        ),
-                        (
-                            level,
-                            'Returning 200 response. To log headers and body, set AZUL_DEBUG to 1.'
-                            if level == INFO else
-                            'Returning 200 response with headers ' +
-                            json.dumps(response_headers) + '. ' +
-                            'See next line for the first 1024 characters of the body.\n' +
-                            '{"up": true}'
-                        )
-                    ])
+                    self.assertEqual(
+                        [
+                            (
+                                INFO,
+                                "Received GET request for '/health/basic', "
+                                f'with {json.dumps(dict(query=None, headers=request_headers))}.'),
+                            (
+                                INFO,
+                                "Authenticated request as OAuth2(access_token='foo_token')"
+                                if authenticated else
+                                'Did not authenticate request.'
+                            ),
+                            (
+                                level,
+                                'Returning 200 response. To log headers and body, set AZUL_DEBUG to 1.'
+                                if level == INFO else
+                                'Returning 200 response with headers ' +
+                                json.dumps(response_headers) + '. ' +
+                                'See next line for the first 1024 characters of the body.\n' +
+                                '{"up": true}'
+                            )
+                        ],
+                        logs
+                    )
