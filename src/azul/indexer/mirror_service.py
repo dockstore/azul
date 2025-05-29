2f49d78872c9b09a4c9c8668afc095c819c4a27b
Noa Aviel Dove <nadove@ucsc.edu>
2025-05-29 14:39:34 -0700

    Convert content type assertion to warning (#7130)

--- src/azul/indexer/mirror_service.py
+++ src/azul/indexer/mirror_service.py
@@ -174,12 +174,9 @@ class BaseMirrorService:
             return None
         else:
             json_content = json.loads(content)
-            # FIXME: Convert to a warning
-            #        https://github.com/DataBiosphere/azul/issues/7130
-            if False:
-                content_type = json_content['content-type']
-                assert content_type == file.content_type, R(
-                    'Conflicting content type', content_type, file)
+            content_type = json_content['content-type']
+            if content_type != file.content_type:
+                log.warning('Conflicting content type %r for file %r', content_type, file)
             return json_content
 
     def mirror_object_key(self, file: File) -> str:
