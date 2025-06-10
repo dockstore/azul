2fd37d602ce8b027c3b63bfe643d8e1d7f113d9a
Abraham Chavez <achave11@ucsc.edu>
2025-06-10 11:18:31 -0700

    Update Python to 3.12.11 (#7209)

--- environment.py
+++ environment.py
@@ -250,7 +250,7 @@ def env() -> Mapping[str, Optional[str]]:
         # and committing the resulting changes. It also requires redeploying the
         # `shared` component.
         #
-        'azul_python_version': '3.12.10',
+        'azul_python_version': '3.12.11',
 
         # The version of Terraform used throughout the system.
         #
