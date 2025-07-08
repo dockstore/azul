f14ce12ffe57033167b4ff285632ffd9e5167464
Abraham Chavez <achave11@ucsc.edu>
2025-07-08 13:40:14 -0700

    [H] Workaround: Two AnVIL snapshots with null in anvil_file.file_size column (#7243)

--- test/integration_test.py
+++ test/integration_test.py
@@ -11,6 +11,7 @@ from concurrent.futures.thread import (
 )
 from contextlib import (
     contextmanager,
+    nullcontext,
 )
 import csv
 import gzip
@@ -588,11 +589,12 @@ class IndexingIntegrationTest(IntegrationTestCase, AlwaysTearDownTestCase):
                         responses.append(response)
                         return response
 
+                    is_anvil = config.is_anvil_enabled(catalog)
                     with (
                         mock.patch.object(self, '_get_url', new=get_url),
                         # Include MA files to reduce the chances of an empty
                         # manifest due to files not matching the filter
-                        self._service_account_credentials
+                        self._service_account_credentials if is_anvil else nullcontext()
                     ):
                         # Make multiple identical concurrent requests to test
                         # the idempotence of manifest generation, and its
