6b663e6bba731104a06c61e83722e6c320e121f4
Abraham Chavez <achave11@ucsc.edu>
2025-07-16 09:38:31 -0700

    Update PyCharm to 2024.3.6-57 (#7257)

--- environment.py
+++ environment.py
@@ -296,7 +296,7 @@ def env() -> Mapping[str, Optional[str]]:
                 'url': 'https://hub.docker.com/_/python',
             },
             'pycharm': {
-                'ref': 'docker.io/ucscgi/azul-pycharm:2024.3.6-56',
+                'ref': 'docker.io/ucscgi/azul-pycharm:2024.3.6-57',
                 'url': 'https://hub.docker.com/repository/docker/ucscgi/azul-pycharm',
                 'is_custom': True
             },
