91cf43be434b3df8831694f250fac294f68c12b5
Abraham Chavez <achave11@ucsc.edu>
2025-07-22 15:31:36 -0700

    Generalize and rename snapshot validation script (#7139)

--- scripts/find_in_snapshots.py
+++ scripts/find_in_snapshots.py
@@ -34,7 +34,6 @@ configure_script_logging(log)
 
 
 def main(args):
-    joiner = '||'
     declined_snapshots = list()
 
     azul = AzulClient(num_workers=1)
@@ -43,7 +42,7 @@ def main(args):
     for catalog, sources in sources_by_catalog.items():
         sources -= previous_sources
         tdr_plugin = azul.repository_plugin(catalog)
-        log.info('Checking for %r in catalog %s', joiner, catalog)
+        log.info('Checking for %r in catalog %s', args.match, catalog)
         if args.snapshot is not None:
             sources = {s for s in sources if args.snapshot in s}
         for spec in sources:
@@ -62,7 +61,7 @@ def main(args):
                     query = f'''
                             SELECT datarepo_row_id, {column}
                             FROM {backtick(table)}
-                            WHERE CONTAINS_SUBSTR({column}, '{joiner}')
+                            WHERE CONTAINS_SUBSTR({column}, '{args.match}')
                             '''
                     result = tdr_plugin._run_sql(query)
                     for row in result:
@@ -101,6 +100,10 @@ if __name__ == '__main__':
                         ],
                         choices=config.catalogs,
                         help='The names of the catalogs to validate.')
+    parser.add_argument('--match',
+                        metavar='STR_MATCH',
+                        default='||',
+                        help='The string pattern to match.')
     parser.add_argument('--snapshot',
                         required=False,
                         metavar='SNAPSHOT_SEQ',
