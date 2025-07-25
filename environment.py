92ab9d625b2288a9d37565efdf3f1c2a8780eb16
Daniel Sotirhos <dsotirho@ucsc.edu>
2025-07-25 15:17:39 -0700

    Update Elasticsearch to 7.17.28-49

--- environment.py
+++ environment.py
@@ -301,7 +301,7 @@ def env() -> Mapping[str, Optional[str]]:
                 'is_custom': True
             },
             'elasticsearch': {
-                'ref': 'docker.io/ucscgi/azul-elasticsearch:7.17.28-48',
+                'ref': 'docker.io/ucscgi/azul-elasticsearch:7.17.28-49',
                 'url': 'https://hub.docker.com/repository/docker/ucscgi/azul-elasticsearch',
                 'is_custom': True
             },
