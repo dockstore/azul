7bc43be2cf531f7bf557bac781bd912eb1c05736
Noa Aviel Dove <nadove@ucsc.edu>
2025-07-31 19:55:35 -0700

    Rename and inline variables

--- scripts/find_in_snapshots.py
+++ scripts/find_in_snapshots.py
@@ -37,56 +37,52 @@ configure_script_logging(log)
 
 
 def main(args):
-    declined_snapshots = list()
+    invalid_sources = list()
 
     azul = AzulClient(num_workers=1)
     sources_by_catalog = azul.matching_sources(args.catalogs, set(args.sources))
     previous_sources: set[str] = set()
     for catalog, sources in sources_by_catalog.items():
         sources -= previous_sources
-        tdr_plugin = azul.repository_plugin(catalog)
-        assert isinstance(tdr_plugin, TDRPlugin)
+        plugin = azul.repository_plugin(catalog)
+        assert isinstance(plugin, TDRPlugin)
         log.info('Checking for %r in catalog %s', args.match, catalog)
-        for spec in sources:
-            log.info('Validating snapshot %s', spec)
-            source = TDRSourceSpec.parse(spec)
-            tables = tdr_plugin._full_table_name(source, 'INFORMATION_SCHEMA.COLUMNS')
+        for source_str in sources:
+            log.info('Validating snapshot %s', source_str)
+            source = TDRSourceSpec.parse(source_str)
             query = f'''
                 SELECT table_name, column_name
-                FROM {backtick(tables)}
+                FROM {backtick(plugin._full_table_name(source, 'INFORMATION_SCHEMA.COLUMNS'))}
             '''
-            rows = tdr_plugin._run_sql(query)
             table_columns = defaultdict(list)
-            for row in rows:
+            for row in plugin._run_sql(query):
                 table_name, column_name = row['table_name'], row['column_name']
                 assert isinstance(table_name, str), table_name
                 assert isinstance(column_name, str), column_name
                 table_columns[table_name].append(column_name)
             for table_name, columns in table_columns.items():
                 log.info('Validating table %s', table_name)
-                table = tdr_plugin._full_table_name(source, table_name)
                 for column in columns:
                     query = f'''
                         SELECT datarepo_row_id, {column}
-                        FROM {backtick(table)}
+                        FROM {backtick(plugin._full_table_name(source, table_name))}
                         WHERE CONTAINS_SUBSTR({column}, {args.match!r})
                     '''
-                    result = tdr_plugin._run_sql(query)
-                    for row in result:
+                    for row in plugin._run_sql(query):
                         match = {
                             'catalog': catalog,
-                            'spec': spec,
+                            'spec': source_str,
                             'table': table_name,
                             'column': column,
                             'row_id': row['datarepo_row_id'],
                             'value': row[column]
                         }
                         log.warning('Undesired string found: %r', match)
-                        declined_snapshots.append(match)
+                        invalid_sources.append(match)
         previous_sources = sources
     print()
-    if declined_snapshots:
-        print(json.dumps(declined_snapshots, indent=4))
+    if invalid_sources:
+        print(json.dumps(invalid_sources, indent=4))
     else:
         print('Checked snapshots OK')
 
