6b69462f6523627163ed5847e36743f1b59253f9
Daniel Sotirhos <dsotirho@ucsc.edu>
2025-09-05 16:52:15 -0700

    Update GitLab to 18.3.2 and runner to 18.3.1 (#7379)

--- environment.py
+++ environment.py
@@ -321,11 +321,11 @@ def env() -> Mapping[str, Optional[str]]:
                 'url': 'https://hub.docker.com/r/clamav/clamav'
             },
             'gitlab': {
-                'ref': 'docker.io/gitlab/gitlab-ce:18.3.1-ce.0',
+                'ref': 'docker.io/gitlab/gitlab-ce:18.3.2-ce.0',
                 'url': 'https://hub.docker.com/r/gitlab/gitlab-ce'
             },
             'gitlab_runner': {
-                'ref': 'docker.io/gitlab/gitlab-runner:ubuntu-v18.3.0',
+                'ref': 'docker.io/gitlab/gitlab-runner:ubuntu-v18.3.1',
                 'url': 'https://hub.docker.com/r/gitlab/gitlab-runner'
             },
             'dind': {
