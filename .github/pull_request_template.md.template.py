772838cc001fc20fd888f55aefa9ac91a37f6ef2
Hannes Schmidt <hannes@ucsc.edu>
2025-09-23 10:30:12 -0700

    Remove mention of sprint for now

--- .github/pull_request_template.md.template.py
+++ .github/pull_request_template.md.template.py
@@ -356,11 +356,6 @@ def emit(t: T, target_branch: str):
                 'type': 'cli',
                 'content': f'PR title references {t.issues('all', 'the')} connected {t.issues}'
             }),
-            iif(t is T.promotion, {
-                'type': 'cli',
-                'content': 'The promoted issues are part of the same sprint as the connected '
-                           'issue'
-            }),
             *(
                 [
                     {
