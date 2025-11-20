340a9d790ce4d2072dbdfef572fb58518a382878
Abraham Chavez <achave11@ucsc.edu>
2025-11-20 15:52:25 -0800

    Update BigQuery Emulator to 0.4.4-48 (#7565)

--- environment.py
+++ environment.py
@@ -286,7 +286,7 @@ def env() -> Mapping[str, str | None]:
                 'is_custom': False
             },
             'bigquery_emulator': {
-                'ref': 'docker.io/ucscgi/azul-bigquery-emulator:0.4.4-47',
+                'ref': 'docker.io/ucscgi/azul-bigquery-emulator:0.4.4-48',
                 'url': 'https://hub.docker.com/repository/docker/ucscgi/azul-bigquery-emulator',
                 'is_custom': True
             },
