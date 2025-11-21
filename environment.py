3ff83264c025cd903e99d9d17d4f472b7359ecd7
Abraham Chavez <achave11@ucsc.edu>
2025-11-20 16:06:28 -0800

    Update ClamAV to 1.5.1-10 (#7565)

--- environment.py
+++ environment.py
@@ -293,7 +293,7 @@ def env() -> Mapping[str, str | None]:
             # Updating any of the four images below additionally requires
             # redeploying the `gitlab` TF component.
             'clamav': {
-                'ref': 'docker.io/clamav/clamav:1.5.1-8',
+                'ref': 'docker.io/clamav/clamav:1.5.1-10',
                 'url': 'https://hub.docker.com/r/clamav/clamav'
             },
             'gitlab': {
