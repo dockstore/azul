56b87a5c133a7a2453b020658f0745a9becc5dc6
Abraham Chavez <achave11@ucsc.edu>
2025-11-21 13:53:04 -0800

    [r] Make dcp55 the default catalog on prod (#7590)

--- deployments/prod/environment.py
+++ deployments/prod/environment.py
@@ -1870,7 +1870,6 @@ def env() -> Mapping[str, str | None]:
                                                     repository=dict(name='tdr_hca')),
                                        sources=condense(sources))
             for atlas, catalog, sources, mirror_limit, it_mirror_limit in [
-                ('hca', 'dcp54', dcp54_sources, None, 1.5 * 1024 ** 3),
                 ('hca', 'dcp55', dcp55_sources, None, 1.5 * 1024 ** 3),
                 ('lungmap', 'lm9', lm9_sources, -1, -1)
             ]
