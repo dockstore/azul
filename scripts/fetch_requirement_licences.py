146dbf604f3cd1b091e432cb4ece8081506f979c
Daniel Sotirhos <dsotirho@ucsc.edu>
2026-01-22 17:25:59 -0800

    Refactor fetch license files script

--- scripts/fetch_requirement_licences.py
+++ scripts/fetch_requirement_licences.py
@@ -12,6 +12,7 @@ Recommended usage when updating the current set of license files:
 4) Delete the old license files.
 """
 import json
+import logging
 from typing import (
     Sequence,
 )
@@ -24,80 +25,94 @@ from urllib3 import (
 )
 
 from azul import (
+    cached_property,
     config,
     require,
 )
 from azul.http import (
     http_client,
 )
+from azul.logging import (
+    configure_script_logging,
+)
+
+log = logging.getLogger(__name__)
 
 
-def github_urls(urls: Sequence[str]) -> set[str]:
-    """
-    Return the GitHub URLs from the list of URLs given.
-    """
-    urls_ = set()
-    for url in urls:
-        url_ = furl(url.rstrip('/'))
-        if url_.netloc == 'github.com':
-            last_segment = url_.path.segments[-1] if url_.path.segments else ''
-            if last_segment == 'issues':
-                # https://github.com/USER/PACKAGE/issues
-                url_.path.segments.pop()
-            elif last_segment.endswith('.git'):
-                # https://github.com/googleapis/proto-plus-python.git
-                url_.path.segments[-1] = last_segment[:-4]
-            urls_.add(str(url_))
-    return urls_
+class Main:
+    destination_path = f'{config.project_root}/docs/licenses/python/'
 
+    file_names = [
+        'LICENSE',
+        'LICENSE.txt',
+        'LICENSE.rst',
+        'LICENSE.md',
+        'LICENSE.mit',
+        'COPYING',
+        'COPYING.BSD',
+        'LICENCE',
+        'LICENCE.md'
+    ]
 
-destination_path = f'{config.project_root}/docs/licenses/python/'
+    @cached_property
+    def http(self):
+        return http_client()
 
-license_file_names = [
-    'LICENSE',
-    'LICENSE.txt',
-    'LICENSE.rst',
-    'LICENSE.md',
-    'LICENSE.mit',
-    'COPYING',
-    'COPYING.BSD',
-    'LICENCE',
-    'LICENCE.md'
-]
+    def main(self):
+        failures = []
 
-http = http_client()
+        with open(f'{config.project_root}/requirements.all.txt', 'r') as f:
+            lines = f.readlines()
 
-with open(f'{config.project_root}/requirements.all.txt', 'r') as f:
-    lines = f.readlines()
-
-failures = []
-for line in lines:
-    if line:
-        package, version = line.split('==')
-        pypi_url = f'https://pypi.org/pypi/{package}'
-        response = http.request('GET', f'{pypi_url}/json')
-        assert isinstance(response, HTTPResponse)
-        require(response.status == 200, response)
-        urls = json.loads(response.data)['info']['project_urls']
-        found = False
-        for url in github_urls(urls.values()):
-            for filename in license_file_names:
-                response = http.request('GET', f'{url}/raw/HEAD/{filename}')
+        for line in lines:
+            if line:
+                package, version = line.split('==')
+                pypi_url = f'https://pypi.org/pypi/{package}/json'
+                response = self.http.request('GET', pypi_url)
                 assert isinstance(response, HTTPResponse)
-                if response.status == 200:
-                    file_path = f'{destination_path}{package}.txt'
-                    with open(file_path, 'wb') as f:
-                        f.write(f'{url}/{filename}\n\n'.encode('ascii'))
-                        f.write(response.data)
-                    print(package, '... done.')
-                    found = True
-                    break
-            if found:
-                break
-        else:
-            failures.append(package)
-            print(package, '... FAIL', pypi_url)
+                require(response.status == 200, response)
+                urls = json.loads(response.data)['info']['project_urls']
+                found = False
+                for url in github_urls(urls.values()):
+                    for filename in self.file_names:
+                        response = self.http.request('GET', f'{url}/raw/HEAD/{filename}')
+                        assert isinstance(response, HTTPResponse)
+                        if response.status == 200:
+                            file_path = f'{self.destination_path}{package}.txt'
+                            with open(file_path, 'wb') as f:
+                                f.write(f'{url}/{filename}\n\n'.encode('ascii'))
+                                f.write(response.data)
+                            log.info('%s... SUCCESS', package)
+                            found = True
+                            break
+                    if found:
+                        break
+                else:
+                    failures.append(package)
+                    log.info('%s... FAIL (%s)', package, pypi_url)
 
-if failures:
-    print()
-    raise Exception(f'Python package license files not found: {failures}')
+        if failures:
+            log.error('Failed to fetch licenses for packages: %s', failures)
+
+    def github_urls(self, urls: Sequence[str]) -> set[str]:
+        """
+        Return URLs to GitHub project home directories found in the URLs given.
+        """
+        urls_ = set()
+        for url in urls:
+            url_ = furl(url.rstrip('/'))
+            if url_.netloc == 'github.com':
+                last_segment = url_.path.segments[-1] if url_.path.segments else ''
+                if last_segment == 'issues':
+                    # https://github.com/USER/PACKAGE/issues
+                    url_.path.segments.pop()
+                elif last_segment.endswith('.git'):
+                    # https://github.com/googleapis/proto-plus-python.git
+                    url_.path.segments[-1] = last_segment[:-4]
+                urls_.add(str(url_))
+        return urls_
+
+
+if __name__ == '__main__':
+    configure_script_logging(log)
+    Main().main()
