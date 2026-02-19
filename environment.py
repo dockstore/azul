0246e724f72b755517cf5b6abab257a857309c58
Daniel Sotirhos <dsotirho@ucsc.edu>
2026-02-19 13:58:20 -0800

    Update GitLab to 18.9.0-ce.0 and runner to v18.9.0 (#7786)

--- environment.py
+++ environment.py
@@ -286,11 +286,11 @@ def env() -> Mapping[str, str | None]:
                 'url': 'https://hub.docker.com/r/clamav/clamav'
             },
             'gitlab': {
-                'ref': 'docker.io/gitlab/gitlab-ce:18.8.4-ce.0',
+                'ref': 'docker.io/gitlab/gitlab-ce:18.9.0-ce.0',
                 'url': 'https://hub.docker.com/r/gitlab/gitlab-ce'
             },
             'gitlab_runner': {
-                'ref': 'docker.io/gitlab/gitlab-runner:ubuntu-v18.8.0',
+                'ref': 'docker.io/gitlab/gitlab-runner:ubuntu-v18.9.0',
                 'url': 'https://hub.docker.com/r/gitlab/gitlab-runner'
             },
             'dind': {
