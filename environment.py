c35b3ac4daa33e29c83249713c7f7a12dd8ccfb4
Abraham Chavez <achave11@ucsc.edu>
2026-03-31 15:09:37 -0700

    Update GitLab to 18.10.1-ce.0 and runner to v18.10.0 (#7898)

--- environment.py
+++ environment.py
@@ -286,11 +286,11 @@ def env() -> Mapping[str, str | None]:
                 'url': 'https://hub.docker.com/r/clamav/clamav'
             },
             'gitlab': {
-                'ref': 'docker.io/gitlab/gitlab-ce:18.9.2-ce.0',
+                'ref': 'docker.io/gitlab/gitlab-ce:18.10.1-ce.0',
                 'url': 'https://hub.docker.com/r/gitlab/gitlab-ce'
             },
             'gitlab_runner': {
-                'ref': 'docker.io/gitlab/gitlab-runner:ubuntu-v18.9.0',
+                'ref': 'docker.io/gitlab/gitlab-runner:ubuntu-v18.10.0',
                 'url': 'https://hub.docker.com/r/gitlab/gitlab-runner'
             },
             'dind': {
