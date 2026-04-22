0ea81e17ece7d2e45b455eb0b2e1ab122f6b556a
Hannes Schmidt <hannes@ucsc.edu>
2026-04-22 12:24:52 -0700

    Reorder methods

--- scripts/provision_credentials.py
+++ scripts/provision_credentials.py
@@ -51,15 +51,6 @@ class CredentialsProvisioner:
     def provision_hmac(self, args):
         self._provision_hmac(args.create)
 
-    def _provision_hmac(self, create):
-        secret_name = config.secrets_manager_secret_name('indexer', 'hmac')
-        if create:
-            self._create_secret(secret_name)
-            if not self._secret_is_stored(secret_name):
-                self._write_secret_value(secret_name, self._random_hmac_key())
-        else:
-            self._destroy_secret(secret_name)
-
     def _provision_sa(self, create, email, secret_name):
         secret_name = config.secrets_manager_secret_name(secret_name)
         if create:
@@ -71,27 +62,50 @@ class CredentialsProvisioner:
             self._destroy_sa_credentials(email, secret_name)
             self._destroy_secret(secret_name)
 
+    def _create_sa_credentials(self, email):
+        iam = self._google_iam
+        key_name = 'projects/-/serviceAccounts/' + email
+        keys = iam.projects().serviceAccounts().keys()
+        key = keys.create(name=key_name, body={}).execute()
+        log.info('Successfully created service account key for user %r', email)
+        return parse_google_key(key)
+
+    def _destroy_sa_credentials(self, service_account_email, secret_name):
+        try:
+            creds = self._secrets_manager.get_secret_value(
+                SecretId=config.secrets_manager_secret_name(secret_name)
+            )
+        except self._secrets_manager.exceptions.ResourceNotFoundException:
+            log.info('Secret already deleted, cannot get key_id for %s',
+                     service_account_email)
+            return
+        else:
+            key_id = json.loads(creds['SecretString'])['private_key_id']
+            iam = self._google_iam
+            try:
+                key_name = f'projects/-/serviceAccounts/{service_account_email}/keys/{key_id}'
+                iam.projects().serviceAccounts().keys().delete(name=key_name).execute()
+            except HttpError as e:
+                if e.resp.reason != 'Not Found':
+                    raise
+            log.info('Successfully deleted service account key with id %r for user %r',
+                     key_id, service_account_email)
+
+    def _provision_hmac(self, create):
+        secret_name = config.secrets_manager_secret_name('indexer', 'hmac')
+        if create:
+            self._create_secret(secret_name)
+            if not self._secret_is_stored(secret_name):
+                self._write_secret_value(secret_name, self._random_hmac_key())
+        else:
+            self._destroy_secret(secret_name)
+
     def _random_hmac_key(self):
         # Even though an HMAC key can be any sequence of bytes, we restrict to
         # base64 in order to encode as string
         key = base64.encodebytes(os.urandom(48)).decode().replace('=', '').replace('\n', '')
         assert len(key) == 64
         return json.dumps({'key': key, 'key_id': str(uuid.uuid4())})
-
-    def _write_secret_value(self, name, value):
-        self._secrets_manager.put_secret_value(
-            SecretId=name,
-            SecretString=value
-        )
-        log.info('Successfully wrote value to AWS secret %r.', name)
-
-    def _create_secret(self, name):
-        try:
-            self._secrets_manager.create_secret(Name=name)
-        except self._secrets_manager.exceptions.ResourceExistsException:
-            log.info('AWS secret %s already exists.', name)
-        else:
-            log.info('AWS secret %s created.', name)
 
     def _secret_is_stored(self, name):
         try:
@@ -103,13 +117,20 @@ class CredentialsProvisioner:
         except KeyError:
             return False
 
-    def _create_sa_credentials(self, email):
-        iam = self._google_iam
-        key_name = 'projects/-/serviceAccounts/' + email
-        keys = iam.projects().serviceAccounts().keys()
-        key = keys.create(name=key_name, body={}).execute()
-        log.info('Successfully created service account key for user %r', email)
-        return parse_google_key(key)
+    def _create_secret(self, name):
+        try:
+            self._secrets_manager.create_secret(Name=name)
+        except self._secrets_manager.exceptions.ResourceExistsException:
+            log.info('AWS secret %s already exists.', name)
+        else:
+            log.info('AWS secret %s created.', name)
+
+    def _write_secret_value(self, name, value):
+        self._secrets_manager.put_secret_value(
+            SecretId=name,
+            SecretString=value
+        )
+        log.info('Successfully wrote value to AWS secret %r.', name)
 
     def _destroy_secret(self, secret_name):
         try:
@@ -142,27 +163,6 @@ class CredentialsProvisioner:
                         log.info('Secret %r not yet deleted. Will keep checking for %.3fs.',
                                  secret_name, deadline - now)
                         time.sleep(5)
-
-    def _destroy_sa_credentials(self, service_account_email, secret_name):
-        try:
-            creds = self._secrets_manager.get_secret_value(
-                SecretId=config.secrets_manager_secret_name(secret_name)
-            )
-        except self._secrets_manager.exceptions.ResourceNotFoundException:
-            log.info('Secret already deleted, cannot get key_id for %s',
-                     service_account_email)
-            return
-        else:
-            key_id = json.loads(creds['SecretString'])['private_key_id']
-            iam = self._google_iam
-            try:
-                key_name = f'projects/-/serviceAccounts/{service_account_email}/keys/{key_id}'
-                iam.projects().serviceAccounts().keys().delete(name=key_name).execute()
-            except HttpError as e:
-                if e.resp.reason != 'Not Found':
-                    raise
-            log.info('Successfully deleted service account key with id %r for user %r',
-                     key_id, service_account_email)
 
 
 if __name__ == '__main__':
